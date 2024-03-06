09ed9b6d5b4e7f65ebd99c8d5cd4d7cadc64fded
kelvinOBA <obamakel1@gmail.com>
2024-03-06 15:13:40 +0100

    Add styled-component, Modify parent component

--- src/components/ParentContainer.tsx
+++ src/components/ParentContainer.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+"use client";
 
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background-color: #fff;
+    max-width: 1024px;
+    margin: 0 auto;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 15px;
+`;
 
 const ParentContainer: React.FC = () => {
-  return (
-    <div >
-   
-    </div>
-  );
+    return <Container></Container>;
 };
 
 export default ParentContainer;
